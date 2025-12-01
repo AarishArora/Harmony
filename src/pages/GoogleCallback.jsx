50c38e26193ef1e8cf51cc756fb3b90b1dbc28f0
Aarish Kumar <aarisharora777@gmail.com>
2025-12-01 21:24:46 +0530

    GoogleCallback error

--- src/pages/GoogleCallback.jsx
+++ src/pages/GoogleCallback.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function GoogleCallback() {
-  const navigate = useNavigate();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -10,15 +9,13 @@ export default function GoogleCallback() {
 
     if (token) {
       localStorage.setItem("token", token);
-      // Dispatch custom event to notify Navbar of auth change
       window.dispatchEvent(new Event('authChange'));
-      // Clean up URL and navigate to home
-      window.history.replaceState({}, document.title, "/");
-      navigate("/");
+
+      window.location.href = "/";
     } else {
-      navigate("/login");
+      window.location.href = "/login";
     }
-  }, [navigate]);
+  }, []);
 
   return <div>Logging you in...</div>;
 }
